6ec8d15612240a05b6db18473af1aaa047fd49b9
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-30 12:01:03 -0500

    play again working

--- high_low.php
+++ high_low.php
@@ -2,43 +2,67 @@
 
 // NOTES
 // 	√ - game picks random number between 1 - 100
-// 	- game prompts user to guess the number
-// 	- gmae says higher or lower with each guess
-// 	- if user correct "GOOE GUESS"
+// 	√ - game prompts user to guess the number
+// 	√ - gmae says higher or lower with each guess
+// 	√ - if user correct "GOOE GUESS"
 // HINTS
-// 	- Conditionals and loops
-// 	- random numbers can be made with rand() function
-// 	- use exit(0) to end game
-// 	- if stuck ctrl-c will exit
+// 	√ - Conditionals and loops
+// 	√ - random numbers can be made with rand() function
+// 	√ - use exit(0) to end game
+// 	√ - if stuck ctrl-c will exit
+// EXTRAS
+//	√ - count var to count tries
+//	√ - convert to functions
+//	√ - replay game
+//	- check if it is a int
 
-// =========== VARIABLES =================
-$count = 0;
 
-// =========== RANDOM NUMBER GEN =================
-				// min num, high num
-$randomNumber = rand(1, 100) . PHP_EOL;
 
-echo $randomNumber;
+
 // =========== GAME =================
-echo "Can You Guess My Number?" . PHP_EOL;
-fwrite(STDOUT, "I'll give you a hint, it is between 1 and 100!" . PHP_EOL);
-$guess = fgets(STDIN);
+function game() {
+	// game intro
+	// random number gen
+	$randomNumber = rand(1, 100) . PHP_EOL;
+	$count = 0;
+	
+	echo "Can You Guess My Number?" . PHP_EOL;
+	fwrite(STDOUT, "I'll give you a hint, it is between 1 and 100!" . PHP_EOL);
+	$guess = fgets(STDIN);
 
-do {
+	// checks if first guess is correct then moves to higher/lower until correct
+	do {
+		if ($randomNumber == $guess) {
+			echo "YOU GUESSED IT!!". PHP_EOL;
+			playAgain();
+		} else if ($randomNumber > $guess) {
+			echo "Your guess was too low. Try again." . PHP_EOL;
+			$guess = fgets(STDIN);
+			$count += 1;
+		} else if ($randomNumber < $guess) {
+			echo "Your guess was too high. Try again." . PHP_EOL;
+			$guess = fgets(STDIN);
+			$count += 1;
+		}
+	} while ($randomNumber != $guess);
+
+	// when guess is correct 
 	if ($randomNumber == $guess) {
-		echo "YOU GUESSED IT!!". PHP_EOL;
-	} else if ($randomNumber > $guess) {
-		echo "Your guess was too low. Try again." . PHP_EOL;
-		$guess = fgets(STDIN);
-		$count += 1;
-	} else {
-		echo "Your guess was too high. Try again." . PHP_EOL;
-		$guess = fgets(STDIN);
-		$count += 1;
+		echo "YOU GUESSED IT....FINALLY AFTER $count TRIES!!". PHP_EOL;
+		playAgain();
 	}
-} while ($randomNumber != $guess);
-
-if ($randomNumber == $guess) {
-	echo "YOU GUESSED IT....FINALLY AFTER $count!!". PHP_EOL;
 }
 
+function playAgain() {
+	fwrite(STDOUT, "Would you like to play agian? Y or N" . PHP_EOL);
+	$playAgain = trim(fgets(STDIN));
+	if ($playAgain == "y" || $playAgain == "Y") {
+		echo "\n";
+		game();
+	} else {
+		echo "Bye Felicia!" . PHP_EOL;
+		exit(0);
+	}
+}
+
+game();
