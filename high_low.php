5a5cc96c7b1c0b867b3d5f127028e183af289f79
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-30 10:17:08 -0500

    notes added

---
+++ high_low.php
@@ -0,0 +1,13 @@
+<?php 
+
+// NOTES
+// 	- game picks random number between 1 - 100
+// 	- game prompts user to guess the number
+// 	- gmae says higher or lower with each guess
+// 	- if user correct "GOOE GUESS"
+// HINTS
+// 	- Conditionals and loops
+// 	- random numbers can be made with rand() function
+// 	- use exit(0) to end game
+// 	- if stuck ctrl-c will exit
+// 	
