ae32d9961075da608d500447e64cbad10261dbbb
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-30 10:49:01 -0500

    do while loop working

--- high_low.php
+++ high_low.php
@@ -1,7 +1,7 @@
 <?php 
 
 // NOTES
-// 	- game picks random number between 1 - 100
+// 	√ - game picks random number between 1 - 100
 // 	- game prompts user to guess the number
 // 	- gmae says higher or lower with each guess
 // 	- if user correct "GOOE GUESS"
@@ -10,4 +10,29 @@
 // 	- random numbers can be made with rand() function
 // 	- use exit(0) to end game
 // 	- if stuck ctrl-c will exit
-// 	
+
+
+// =========== RANDOM NUMBER GEN =================
+				// min num, high num
+$randomNumber = rand(1, 100) . PHP_EOL;
+
+echo $randomNumber;
+// =========== GAME =================
+echo "Can You Guess My Number?" . PHP_EOL;
+fwrite(STDOUT, "I'll give you a hint, it is between 1 and 100!" . PHP_EOL);
+$guess = fgets(STDIN);
+
+do {
+	if ($randomNumber == $guess) {
+		echo "YOU GUESSED IT!!". PHP_EOL;
+	} else if ($randomNumber > $guess) {
+		echo "Your guess was too low. Try again." . PHP_EOL;
+		$guess = fgets(STDIN);
+	} else {
+		echo "Your guess was too high. Try again." . PHP_EOL;
+		$guess = fgets(STDIN);
+	}
+} while ($randomNumber != $guess);
+
+
+
